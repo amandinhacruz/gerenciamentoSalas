8326244ca34666bee7f528a1a424a0b2c3b6ce37
amandinhacruz <amandaellem2023@gmail.com>
2025-05-13 10:58:44 -0300

    subindo metodo put

--- src/main/java/com/example/demo/repository/SalasRepository.java
+++ src/main/java/com/example/demo/repository/SalasRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Salas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+
 
 @Repository
 public interface SalasRepository extends JpaRepository<Salas, Long> {
